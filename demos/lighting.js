679b4fbccd2ff8a84f046c55efb9cfbe1f3c57e0
Filipp Keks <filipp@keks.ee>
2022-11-21 21:58:09 +0200

    better ambient

--- demos/lighting.js
+++ demos/lighting.js
@@ -8,9 +8,9 @@ import {positions, normals, indices} from "../blender/monkey.js"
 // ******************************************************
 
 let baseColor = vec3.fromValues(1.0, 0.1, 0.2);
-let ambientLightColor = vec3.fromValues(0.05, 0.05, 0.1);
+let ambientLightColor = vec3.fromValues(0.1, 0.1, 1.0);
 let numberOfPointLights = 2;
-let pointLightColors = [vec3.fromValues(1.0, 1.0, 1.0), vec3.fromValues(0.02, 0.5, 1.0)];
+let pointLightColors = [vec3.fromValues(1.0, 1.0, 1.0), vec3.fromValues(0.02, 0.4, 0.5)];
 let pointLightInitialPositions = [vec3.fromValues(5, 0, 2), vec3.fromValues(-5, 0, 2)];
 let pointLightPositions = [vec3.create(), vec3.create()];
 
@@ -26,9 +26,9 @@ let lightCalculationShader = `
     
     // This function calculates light reflection using Phong reflection model (ambient + diffuse + specular)
     vec4 calculateLights(vec3 normal, vec3 position) {
-        float ambientIntensity = 1.0;
+        float ambientIntensity = 0.5;
         float diffuseIntensity = 1.0;
-        float specularIntensity = 5.0;
+        float specularIntensity = 2.0;
         float specularPower = 100.0;
         float metalness = 0.0;
 
