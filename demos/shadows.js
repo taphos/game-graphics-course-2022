5e72d9d65676d88bb9b42a96f87c2bf0d9d9258a
Filipp Keks <filipp@keks.ee>
2022-12-04 21:46:20 +0200

    removed garbage

--- demos/shadows.js
+++ demos/shadows.js
@@ -3,8 +3,7 @@
 import PicoGL from "../node_modules/picogl/build/module/picogl.js";
 import {mat4, vec3, vec4, quat} from "../node_modules/gl-matrix/esm/index.js";
 
-import {positions, normals, indices} from "../blender/cube.js"
-import {positions as planePositions, normals as planeNormals, indices as planeIndices} from "../blender/plane.js"
+import {positions, normals, indices} from "../blender/cube.js";
 
 // language=GLSL
 let fragmentShader = `
